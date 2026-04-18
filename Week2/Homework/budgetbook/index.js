378e872692a9a7041f3c14f2758d76f28ca317b6
hdg0116 <hdg0116@gmail.com>
2026-04-18 17:39:32 +0900

    feat: 더미데이터 표에 렌더링

--- Week2/Homework/budgetbook/index.js
+++ Week2/Homework/budgetbook/index.js
@@ -1,4 +1,19 @@
 import "./dummy.js";
 
-
+// 더미데이터 불러와서 로컬 스토리지 저장
 const expenseData = JSON.parse(localStorage.getItem("expenseData")) || [];
+
+// 표 가져오기
+const expenseList = document.querySelector('.expense-list');
+
+// 표에 더미데이터 렌더링
+expenseList.innerHTML = expenseData.map((item) => `
+  <tr>
+    <td><input type="checkbox" name="select" value="${item.id}" /></td>
+    <td>${item.title}</td>
+    <td>${item.amount > 0 ? `+${item.amount}` : item.amount}</td>
+    <td>${item.date}</td>
+    <td>${item.category}</td>
+    <td>${item.payment}</td>
+  </tr>
+`).join("");
