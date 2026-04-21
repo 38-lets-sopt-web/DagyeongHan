368937c630abdce132f38d160d854021dc77e54c
hdg0116 <hdg0116@gmail.com>
2026-04-22 00:26:26 +0900

    fix: element 파일 정리

--- Week2/Homework/budgetbook/js/elements.js
+++ Week2/Homework/budgetbook/js/elements.js
@@ -1,42 +1,40 @@
-
 // 헤더 아이콘 요소 선택
 export const headerIcon = document.querySelector("header div");
 
 // 드롭다운 관련 요소 한번에 선택
 export const dropdowns = document.querySelectorAll(".dropdown, .non-label-dropdown, .add-dropdown");
-
-// 표 드롭다운 요소 선택
+// 정렬 드롭다운 요소 선택
 export const sortDropdown = document.querySelector(".non-label-dropdown");
+// 필터 드롭다운 요소 모두 선택
+export const filterDropdowns = document.querySelectorAll("main section .dropdown");
+// 추가 모달 드롭다운 요소 전체 선택
+export const addDropdowns = document.querySelectorAll(".modal .add-dropdown");
 
-// 추가 버튼 요소 선택
-export const addButton = document.querySelector(".add-btn");
-
-// 모달 백드롭 요소 선택
-export const modalBackdrop = document.querySelector(".modal-backdrop");
-
-// 표 가져오기
-export const expenseList = document.querySelector('.expense-list');
-
+// 지출 목록 요소 선택
+export const expenseList = document.querySelector(".expense-list");
+// 총액 요소 선택
+export const totalAmount = document.querySelector(".total-amount");
+// 제목 input 요소 선택
+export const titleInput = document.querySelector(".input input");
 // 전체 선택 체크박스 요소 선택
 export const selectAllCheckbox = document.querySelector("input[name='select-all']");
 
+// 추가 버튼 요소 선택
+export const addButton = document.querySelector(".add-btn");
 // 삭제 버튼 요소 선택
-export const deleteBtn = document.querySelector('.delete-btn');
-
-// 합계 요소 선택
-export const totalAmount = document.querySelector(".total-amount");
-
-// 제목 input 요소 선택
-export const titleInput = document.querySelector(".input input");
-
+export const deleteBtn = document.querySelector(".delete-btn");
 // 적용 버튼 요소 선택
 export const applyBtn = document.querySelector(".apply-btn");
-
 // 초기화 버튼 요소 선택
 export const resetBtn = document.querySelector(".reset-btn");
 
-// 검색 필터 드롭다운 요소 모두 선택
-export const filterDropdowns = document.querySelectorAll("main section .dropdown");
+// 모달 배경 요소 선택
+export const modalBackdrop = document.querySelector(".modal-backdrop");
+// 공통 모달 제목 요소 선택
+export const modalTitle = document.querySelector(".modal-title");
+// 공통 모달 닫기 버튼 요소 선택
+export const modalCloseBtn = document.querySelector(".modal-close-btn");
+
 // 내역 추가 모달 요소 선택
 export const addModalContent = document.querySelector(".modal-content-add");
 // 내역 추가 모달 제목 입력 요소 선택
@@ -45,15 +43,9 @@ export const addTitleInput = document.querySelector(".add-title-input");
 export const addDateInput = document.querySelector(".add-date-input");
 // 내역 추가 모달 금액 입력 요소 선택
 export const addAmountInput = document.querySelector(".add-amount");
-// 내역 추가 모달 드롭다운 요소 전체 선택
-export const addDropdowns = document.querySelectorAll(".modal .add-dropdown");
 
-// 항목 상세 모달 요소 선택
+// 내역 상세 모달 요소 선택
 export const detailModalContent = document.querySelector(".modal-content-detail");
-// 공통 모달 헤더 제목 요소 선택
-export const modalTitle = document.querySelector(".modal-title");
-// 공통 모달 닫기 버튼 요소 선택
-export const modalCloseBtn = document.querySelector(".modal-close-btn");
 // 상세 모달 제목 값 요소 선택
 export const detailTitle = document.querySelector(".detail-title");
 // 상세 모달 금액 값 요소 선택
