302daee85753ac2733e5c6fc7599ed6c9899debc
hdg0116 <hdg0116@gmail.com>
2026-05-07 05:12:11 +0900

    feat: 정보 수정 시 input 입력 검증 후 버튼 활성화 기능 추가

--- week4/homework/member-admin/src/components/mypage/EditMyInfo.tsx
+++ week4/homework/member-admin/src/components/mypage/EditMyInfo.tsx
@@ -1,9 +1,15 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
+import { useState } from "react";
 import Input from "@/components/Input";
 import Button from "@/components/Button";
 
 export default function EditMyInfo() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [age, setAge] = useState("");
+  const isEditEnabled = name.trim() && email.trim() && age.trim();
+
   return (
     <div css={rootContainerStyle}>
       <div css={layoutStyle}>
@@ -11,31 +17,42 @@ export default function EditMyInfo() {
         {/* 아이디 입력 */}
         <label css={fieldStyle}>
           <span css={labelStyle}>이름</span>
-          <Input placeholder="수정할 이름을 입력해주세요." />
+          <Input
+            placeholder="수정할 이름을 입력해주세요."
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          />
         </label>
 
         {/* 비밀번호 입력 */}
         <label css={fieldStyle}>
           <span css={labelStyle}>이메일</span>
-          <Input placeholder="수정할 이메일을 입력해주세요." />
+          <Input
+            placeholder="수정할 이메일을 입력해주세요."
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
         </label>
 
         <label css={fieldStyle}>
           <span css={labelStyle}>나이</span>
-          <Input placeholder="수정할 나이를 입력해주세요." />
+          <Input
+            placeholder="수정할 나이를 입력해주세요."
+            value={age}
+            onChange={(event) => setAge(event.target.value)}
+          />
         </label>
 
       </div>
-      
-      {/* 버튼 */}
-      <Button buttonText="정보 수정" />
 
+      {/* 버튼 */}
+      <Button buttonText="정보 수정" disabled={!isEditEnabled} />
     </div>
-  )
+  );
 }
 
 const rootContainerStyle = css`
-  width: 100%;  
+  width: 100%;
   display: flex;
   flex-direction: column;
   gap: 2em;
