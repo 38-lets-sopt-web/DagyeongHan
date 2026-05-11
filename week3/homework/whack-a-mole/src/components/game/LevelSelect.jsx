ad324c3c6732c284772f02becafa2ab3dd61e3e6
hdg0116 <hdg0116@gmail.com>
2026-05-11 10:08:10 +0900

    refactor: LevelSelect 파일 스타일 분리

--- week3/homework/whack-a-mole/src/components/game/LevelSelect.jsx
+++ week3/homework/whack-a-mole/src/components/game/LevelSelect.jsx
@@ -1,8 +1,7 @@
-/** @jsxImportSource @emotion/react */
 import { useEffect, useRef, useState } from 'react';
-import { css } from '@emotion/react';
 import { DEFAULT_LEVEL, LEVEL_OPTIONS } from '../../constants/game';
 import ChevronDownIcon from '../../assets/ic-chevron-down.svg?react';
+import { arrowStyle, dropdownStyle, menuStyle, optionStyle, triggerStyle } from './LevelSelect.styles';
 
 export default function LevelSelect({ value = DEFAULT_LEVEL, onChange, disabled = false }) {
   const [isOpen, setIsOpen] = useState(false);
@@ -67,69 +66,3 @@ export default function LevelSelect({ value = DEFAULT_LEVEL, onChange, disabled 
     </div>
   );
 }
-
-const dropdownStyle = css`
-  position: relative;
-  width: 7.5em;
-`;
-
-const triggerStyle = (isOpen) => css`
-  width: 100%;
-  box-sizing: border-box;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  gap: 0.6em;
-  padding: 0.5em 1em;
-  border: none;
-  border-radius: 12px;
-  background: #fff;
-  font-size: 1em;
-  font-weight: 700;
-  cursor: pointer;
-  box-shadow: ${isOpen ? '0 0 0 3px rgba(255, 178, 178, 0.35)' : 'none'};
-
-  &:disabled {
-    opacity: 0.5;
-    cursor: not-allowed;
-  }
-`;
-
-const arrowStyle = (isOpen) => css`
-  width: 1.2em;
-  height: 1.2em;
-  transform: ${isOpen ? 'rotate(180deg)' : 'rotate(0deg)'};
-  transition: transform 120ms ease;
-`;
-
-const menuStyle = css`
-  position: absolute;
-  z-index: 10;
-  top: calc(100% + 0.4em);
-  left: 0;
-  width: 100%;
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0.3em;
-  list-style: none;
-  border-radius: 12px;
-  background: #fff;
-  box-shadow: 0 0.5em 1.5em rgba(18, 52, 84, 0.14);
-`;
-
-const optionStyle = (isSelected) => css`
-  width: 100%;
-  padding: 0.25em 0.5em;
-  border: none;
-  border-radius: 9px;
-  background: ${isSelected ? '#FFB2B2' : 'transparent'};
-  color: ${isSelected ? '#fff' : '#123454'};
-  font: inherit;
-  font-weight: 700;
-  text-align: left;
-  cursor: pointer;
-
-  &:hover {
-    background: ${isSelected ? '#FFB2B2' : '#fff2f2'};
-  }
-`;
