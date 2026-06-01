a99ff71cfb92203db9013b9665dc8c962e0a18ef
hdg0116 <hdg0116@gmail.com>
2026-06-02 03:25:22 +0900

    feat: 카드 클릭 시 영화 상세페이지 이동 기능 추가

--- week6/homework/movie-search/src/shared/components/Card.tsx
+++ week6/homework/movie-search/src/shared/components/Card.tsx
@@ -1,21 +1,24 @@
 import type { Movie } from '@/shared/schemas/movie'
+import { Link } from 'react-router'
 
 const IMAGE_BASE_URL = import.meta.env.VITE_IMAGE_BASE_URL
 
 const Card = ({ title, release_date, overview, poster_path }: Movie) => {
   return (
-    <article className="w-full h-147.5 bg-neutral-100 rounded-small overflow-hidden cursor-pointer transition-transform duration-300 ease-[ease] hover:scale-102">
-      <img
-        className="w-full h-[400px] object-cover"
-        src={poster_path ? `${IMAGE_BASE_URL}${poster_path}` : undefined}
-        alt={title}
-      />
-      <div className='flex flex-col gap-2 px-4 py-4'>
-        <h3 className="typo-title1 text-neutral-800">{title}</h3>
-        <p className="typo-body1 text-neutral-800">{release_date}</p>
-        <p className="typo-body1 text-neutral-800 overflow-hidden line-clamp-4 h-24">{overview}</p>
-      </div>
-    </article>
+    <Link to='/movie-detail/:movieId'>
+      <article className="w-full h-147.5 bg-neutral-100 rounded-small overflow-hidden cursor-pointer transition-transform duration-300 ease-[ease] hover:scale-102">
+        <img
+          className="w-full h-[400px] object-cover"
+          src={poster_path ? `${IMAGE_BASE_URL}${poster_path}` : undefined}
+          alt={title}
+        />
+        <div className='flex flex-col gap-2 px-4 py-4'>
+          <h3 className="typo-title1 text-neutral-800">{title}</h3>
+          <p className="typo-body1 text-neutral-800">{release_date}</p>
+          <p className="typo-body1 text-neutral-800 overflow-hidden line-clamp-4 h-24">{overview}</p>
+        </div>
+      </article>
+    </Link>
   )
 }
 
