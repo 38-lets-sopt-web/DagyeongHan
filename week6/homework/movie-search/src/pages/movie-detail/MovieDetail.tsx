b308a40a3cda7717be6505bf0db46e67d762f724
hdg0116 <hdg0116@gmail.com>
2026-06-02 07:45:16 +0900

    fix: 뒤로가기 버튼 구조 변경

--- week6/homework/movie-search/src/pages/movie-detail/MovieDetail.tsx
+++ week6/homework/movie-search/src/pages/movie-detail/MovieDetail.tsx
@@ -4,6 +4,7 @@ import OverviewSection from '@/pages/movie-detail/sections/OverviewSection'
 import InfoSection from '@/pages/movie-detail/sections/InfoSection'
 import RatingSection from '@/pages/movie-detail/sections/RatingSection'
 import { useMovieDetail } from '@/pages/movie-detail/hooks/useMovieDetail'
+import { ArrowLeftIcon } from '@/shared/assets/icons'
 
 export default function MovieDetail() {
   const { movieId } = useParams<{ movieId: string }>()
@@ -14,7 +15,10 @@ export default function MovieDetail() {
 
   return (
     <div className="flex flex-col px-70 py-10 gap-4">
-      <button type="button" onClick={() => navigate(-1)} className="self-start">← 뒤로가기</button>
+      <button type="button" onClick={() => navigate('/')} className="flex items-center gap-1 self-start cursor-pointer">
+        <ArrowLeftIcon aria-hidden width={16} height={16} />
+        <span>목록으로 돌아가기</span>
+      </button>
       <HeroSection data={data} />
       <OverviewSection data={data} />
       <div className="w-full flex gap-4">
--- week6/homework/movie-search/src/shared/assets/icons/index.tsx
+++ week6/homework/movie-search/src/shared/assets/icons/index.tsx
@@ -1,1 +1,2 @@
 export { default as ChevronDownIcon } from './ic-chevron-down.svg?react'
+export { default as ArrowLeftIcon } from './ic-arrow-left.svg?react'
