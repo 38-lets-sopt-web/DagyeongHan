57315ecc0a5c871c4cb48d8f8486186e4a1dde77
hdg0116 <hdg0116@gmail.com>
2026-06-02 08:42:59 +0900

    fix: 사용하지 않는 파일 제거

--- week6/homework/movie-search/src/shared/constants/genres.ts
+++
@@ -1,9 +0,0 @@
-import type { Option } from '@/shared/types/common'
-
-export const GENRE_OPTIONS: Option[] = [
-  { label: '액션', value: 'action' },
-  { label: '코미디', value: 'comedy' },
-  { label: '드라마', value: 'drama' },
-  { label: '공포', value: 'horror' },
-  { label: 'SF', value: 'sci-fi' },
-]
