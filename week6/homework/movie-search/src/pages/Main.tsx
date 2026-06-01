ff53e09cb9a585b63db5397879abb8bce5b18404
hdg0116 <hdg0116@gmail.com>
2026-06-01 16:39:30 +0900

    feat: 영화 목록 조회 API 카드 리스트에 렌더링

--- week6/homework/movie-search/src/pages/Main.tsx
+++ week6/homework/movie-search/src/pages/Main.tsx
@@ -1,13 +1,22 @@
-import DropdownSection from "@/sections/DropdownSection";
-import Card from "@/shared/components/Card";
-import Header from "@/shared/components/Header";
+import DropdownSection from "@/sections/DropdownSection"
+import Card from "@/shared/components/Card"
+import Header from "@/shared/components/Header"
+import { useMovies } from "@/shared/hooks/useMovies"
 
 export default function Main() {
+  const { data } = useMovies()
+
   return (
-    <main className="flex flex-col gap-10 px-80 py-10">
+    <main className="flex flex-col gap-10 px-30 py-10">
       <Header />
       <DropdownSection />
-      <Card />
+      <ul className="flex flex-wrap gap-4">
+        {data?.results.map((movie) => (
+          <li key={movie.id}>
+            <Card {...movie} />
+          </li>
+        ))}
+      </ul>
     </main>
   )
 }
--- week6/homework/movie-search/src/shared/components/Card.tsx
+++ week6/homework/movie-search/src/shared/components/Card.tsx
@@ -1,11 +1,21 @@
-const Card = () => {
+import type { Movie } from '@/shared/schemas/movie'
+
+const IMAGE_BASE_URL = import.meta.env.VITE_IMAGE_BASE_URL
+
+const Card = ({ title, release_date, overview, poster_path }: Movie) => {
   return (
-    <div className="w-50 px-5 py-5 bg-neutral-100 rounded-small">
-      <img className="w-full bg-amber-50" />
-      <h3 className="typo-title1 text-neutral-800">제목</h3>
-      <p className="typo-body1 text-neutral-800">날짜</p>
-      <p className="type-body1 text-neutral-800">설명</p>
-    </div>
+    <article className="w-70 bg-neutral-100 rounded-small overflow-hidden">
+      <img
+        className="w-full h-[400px] object-cover"
+        src={poster_path ? `${IMAGE_BASE_URL}${poster_path}` : undefined}
+        alt={title}
+      />
+      <div className='flex flex-col gap-2 px-4 py-4'>
+        <h3 className="typo-title1 text-neutral-800">{title}</h3>
+        <p className="typo-body1 text-neutral-800">{release_date}</p>
+        <p className="typo-body1 text-neutral-800 overflow-hidden line-clamp-4">{overview}</p>
+      </div>
+    </article>
   )
 }
 
