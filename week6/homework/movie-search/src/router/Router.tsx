c4ef80fe0de2b630856f1adc881e4abc079dbf62
hdg0116 <hdg0116@gmail.com>
2026-06-02 09:25:01 +0900

    fix: 디자인 시스템 파일 제거

--- week6/homework/movie-search/src/router/Router.tsx
+++ week6/homework/movie-search/src/router/Router.tsx
@@ -1,16 +1,11 @@
 import { createBrowserRouter } from 'react-router'
 import Main from '@/pages/main/Main'
-import DesignSystem from '@/pages/DesignSystem'
 import MovieDetail from '@/pages/movie-detail/MovieDetail'
 
 const router = createBrowserRouter([
   {
     path: "/",
     Component: Main,
-  },
-  {
-    path: "/design-system",
-    Component: DesignSystem,
   },
   {
     path: "movie-detail/:movieId",
